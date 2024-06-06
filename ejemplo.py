513e27c2c4768d9f62debf477b87011146c64900
AngelRM18 <2023-40041@liceocanadiense.edu.gt>
2024-06-06 14:13:12 -0600

    se van a subbir 3 proyectos

---
+++ ejemplo.py
@@ -0,0 +1,13 @@
+a = 5
+b = 8
+d = 9
+e = 4
+f = 9
+c = a+b+d+e+f
+q = a-b-d-e-f
+r = a*b*d*e*f
+z = a/b/d/e/f
+print ("la suma de los numeros es: ",c)
+print ("la resta de los numeros es: ",q)
+print ("la multiplicacion de los numeros es: ",r)
+print ("la division de los numeros es: ",z)
