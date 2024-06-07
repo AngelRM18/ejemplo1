49627bc02a9ca9e4f404003b77ec61c5cdca0c83
AngelRM18 <2023-40041@liceocanadiense.edu.gt>
2024-06-07 12:37:55 -0600

    tarea 1 del 07-06-24

---
+++ tarea1.py
@@ -0,0 +1,13 @@
+n1=int(input("ingrese su primer dato \n"))
+n2=int(input("ingrese su segundo dato \n"))
+n3=int(input("ingrese su tercer dato \n"))
+n4=int(input("ingrese su cuarto dato \n"))
+n5=int(input("ingrese su quinto dato \n"))
+suma=n1+n2+n3+n4+n5
+resta=n1-n2-n3-n4-n5
+multi=n1*n2*n3*n4*n5
+divi=+n2/n3/n4/n5
+print("la suma de los datos es: " + str (suma))
+print("la suma de la resta es: " + str (resta))
+print("la suma de la multiplicacion es: " + str (multi))
+print("la suma de los divicion es: " + str (divi))
